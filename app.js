7496f90fff3aa1345814842dd2de532d262fcb1a
wenrou77 <wendyog@gmail.com>
2022-01-16 19:31:32 +0800

    feat: add index handlebars

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+require('./config/mongoose')
+
+const routes = require('./routes')
+const usePassport = require('./config/passport')
+const Todo = require('./models/todo')
+
+const app = express()
+const PORT = process.env.PORT
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+usePassport(app)
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
+app.use(routes)
+
+// 設定 port 
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
---
+++ models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+  isDone: {
+    type: Boolean,
+    default: false
+  },
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  }
+})
+module.exports = mongoose.model('Todo', todoSchema)
