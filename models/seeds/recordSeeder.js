f2b55c07b1a4a546620585a5575c8bf4c792e24c
wenrou77 <wendyog@gmail.com>
2022-02-05 20:20:52 +0800

    modify recordSeeders

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -9,7 +9,7 @@ const recordList = require('../../record.json')
 const categoryList = require('../../category.json')
 const recordNum = recordList.length
 const categoryNum = categoryList.length
-const SEED_USER = 
+const SEED_USER =
 {
   "name": "root",
   "email": "root@example.com",
@@ -17,39 +17,75 @@ const SEED_USER =
 }
 
 const db = require('../../config/mongoose')
-db.once('open', ()=> {
-  Promise.all(
-    bcrypt
+// db.once('open', () => {
+//   Promise.all(Array.from([SEED_USER], SEED_USER => {
+//     return bcrypt
+//       .genSalt(10)
+//       .then(salt => bcrypt.hash(SEED_USER.password, salt))
+//       .then(hash => User.create({
+//         name: SEED_USER.name,
+//         email: SEED_USER.email,
+//         password: hash
+//       }))
+//       .then((user) => {
+//         const userId = user._id
+//         return Category.find()
+//           .lean()
+//           .then((categories) => {
+//             let categoryId = []
+//             Array.from({ length: categoryNum }, (_, i) => recordList.forEach((record) => {
+//               if (record.category === categories[i].name) {
+//                 categoryId.push(categories[i]._id)
+//                 return categoryId
+//               }
+//             })
+//             )
+//             Array.from({ length: recordNum }, (_, i) => {
+//               console.log(recordList[`${i}`].name, ' ', categoryId[`${i}`])
+//               return Record.create({
+//                 name: recordList[`${i}`].name,
+//                 date: recordList[`${i}`].date,
+//                 amount: recordList[`${i}`].amount,
+//                 userId,
+//                 categoryId: categoryId[`${i}`]
+//               })
+//             })
+//           })
+//       })
+//   })).then(() => {
+//     console.log('record seeder done.')
+//     process.exit()
+//   }).catch(error => {
+//     console.log(error)
+//   })
+// })
+
+db.once('open', () => {
+  Promise.all(Array.from([SEED_USER], SEED_USER => {
+    return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(SEED_USER.password, salt))
       .then(hash => User.create({
         name: SEED_USER.name,
         email: SEED_USER.email,
-        password: hash}))
-      .then((user) => {
+        password: hash
+      }))
+      .then(user => {
         const userId = user._id
-        Category.find()
-          .lean()
-          .then((categories) => {
-            let categoryId = []
-            for (var i = 0; i <= categoryNum - 1; i++) {
-              for (let record of recordList) {
-                if (record.category === categories[i].name) {
-                  categoryId.push(categories[i]._id)
-                }
-              }
-            }
-            return Array.from({ length: recordNum }, (_, i) =>
-              Record.create({
-                name: recordList[`${i}`].name,
-                date: recordList[`${i}`].date,
-                amount: recordList[`${i}`].amount,
-                userId, categoryId: categoryId[`${i}`]
-              })
-            )
-          })
+        return Promise.all(Array.from({ length: recordNum }, (_, i) => {
+          return Category.findOne({ name: recordList[i].category })
+            .then(category => {
+              const categoryId = category._id
+              recordList[i].categoryId = categoryId
+              recordList[i].userId = userId
+              return recordList[i]
+            }).then(recordData => {
+              return Record.create(recordData)
+            })
         })
-  ).then(() => {
+        )
+      })
+  })).then(() => {
     console.log('record seeder done.')
     process.exit()
   }).catch(error => {
